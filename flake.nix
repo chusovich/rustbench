faf9f5f9c2c81d867733642ed2dcb4669d5adc2d
Caleb Husovich <calebmhusovich@gmail.com>
2026-03-03 12:56:24 -0500

    finishing guessing game

--- flake.nix
+++ flake.nix
@@ -1,22 +1,20 @@
 {
-  description = "Rust development shell (pinned nixpkgs)";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux"; # change if needed
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.cargo
-          pkgs.rustc
-        ];
-      };
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo rustc rustfmt clippy rust-analyzer
+      ];
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
+
+  };
 }
